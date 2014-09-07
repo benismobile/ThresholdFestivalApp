d79e7bd2d32b544fb38e8e25dffd255ae2be389f
benismobile <vagrant@precise32.(none)>
2014-09-07 10:11:49 +0000

    missing files from obj import work

---
+++ compile_test2.sh
@@ -0,0 +1,2 @@
+javac -classpath ./bin/classes/:/usr/share/java/junit4.jar:/home/vagrant/apps/thresholdfestival/ThresholdFestivalApp/bin:/home/vagrant/Downloads/objimport/library/objimport.jar -d /home/vagrant/apps/thresholdfestival/ThresholdFestivalApp/test/bin  test/src/com/example/android/geofence/WavefrontObjParserTest.java
+
---
+++ runtest3.sh
@@ -0,0 +1,2 @@
+/usr/lib/jvm/java-7-openjdk-i386/bin/java -cp /usr/share/java/junit4.jar:/home/vagrant/Downloads/objimport/library/objimport.jar:/home/vagrant/apps/thresholdfestival/ThresholdFestivalApp/test/bin org.junit.runner.JUnitCore com.example.android.geofence.WavefrontObjParserTest
+
